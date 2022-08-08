64347ad05580246802558620a7cc46beecf5b949
Jim Lao <laojimmarklino@gmail.com>
2022-08-08 22:26:12 +0800

    module

---
+++ src/components/Modal.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import '../css/Modal.module.css'
+import { RiCloseLine } from "react-icons/ri";
+
+
+const Modal = () => {
+    return <h1>Hello Modal</h1>;
+  };
+  
+export default Modal;
