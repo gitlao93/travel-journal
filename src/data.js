df85e52c648fa5b78000c78dd17b3e9659f6a677
gitlao93 <laojimmarklino@gmail.com>
2022-08-08 16:44:32 +0800

    added data

--- src/data.js
+++ src/data.js
@@ -1,12 +1,22 @@
 export default [
+    {
+        key: 3,
+        title: "Boracay Escaped",
+        location: "Philippines",
+        googleMapsUrl: "https://goo.gl/maps/MVkurGimWaUdx6Yc9",
+        startDate: "12 Feb, 2021",
+        endDate: "24 Feb, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://media.istockphoto.com/photos/white-beach-boracay-philippines-picture-id533554773?s=612x612"
+    },
     
     {
         key: 2,
         title: "Palawan Escaped",
         location: "Philippines",
         googleMapsUrl: "https://goo.gl/maps/ejn8QijD6xU7UKdJA",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
+        startDate: "12 Jan, 2022",
+        endDate: "24 Jan, 2022",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         imageUrl: "https://images.unsplash.com/photo-1518509562904-e7ef99cdcc86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
     },
